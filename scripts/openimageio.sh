2b5beef74cf5dc58d6079d9cf8a036885e36ec7b
woong <hello@lazypic.org>
2019-02-08 11:17:39 +0900

    add root option

--- scripts/openimageio.sh
+++ scripts/openimageio.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run as Root" 1>&2
+	exit 1
+fi
+
 yum -y install OpenImageIO
 yum -y install OpenImageIO-iv
 yum -y install OpenImageIO-devel
