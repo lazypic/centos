69baec8ccc2a585b6904b2c6a86200e11690e774
woong <hello@lazypic.org>
2019-02-08 16:51:37 +0900

    add pip util

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -5,3 +5,6 @@ python get-pip.py
 
 #pip install --upgrade setuptools # 간혹 이 작업 이후 부팅이 안되는 컴퓨터가 발생함.
 pip install --user pymediainfo
+pip install --user PySide2
+pip install --user numpy
+pip install --user Cython
