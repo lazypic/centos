f3094fc8d6dbdc177c0382a9bdd55d3732f1a8ab
woong <hello@lazypic.org>
2019-02-14 08:37:54 +0900

    edit link

--- scripts/blender.sh
+++ scripts/blender.sh
@@ -3,5 +3,5 @@ if [ ! -d "/home/$USER/app" ]; then
 	mkdir /home/$USER/app
 fi
 cd /home/$USER/app
-wget https://www.blender.org/download/Blender2.79/blender-2.79b-linux-glibc219-x86_64.tar.bz2
+wget https://download.blender.org/release/Blender2.79/blender-2.79b-linux-glibc219-x86_64.tar.bz2
 tar -xjf blender-2.79b-linux-glibc219-x86_64.tar.bz2
