9b067f10c30ee5195ae5480d771812dad8ca5d7e
kim hanwoong <khw7096@gmail.com>
2019-02-13 19:10:44 +0900

    add cp bin

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 echo "source /home/$USER/centos/env/init.env" >> /home/$USER/.bashrc
 echo "source /home/$USER/centos/env/help.env" >> /home/$USER/.bashrc
+
+if [ ! -d "$HOME/bin" ]; then
+	mkdir $HOME/bin
+fi
+cp ./bin/* $HOME/bin
