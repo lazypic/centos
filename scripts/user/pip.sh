c990d3af041f45f5ce288501fa25b85f012baaea
woong <hello@lazypic.org>
2019-02-26 14:55:19 +0900

    user, root 로 분리함.

--- scripts/user/pip.sh
+++ scripts/user/pip.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-cd /tmp
-curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-python get-pip.py
-
 #pip install --upgrade setuptools # 간혹 이 작업 이후 부팅이 안되는 컴퓨터가 발생함.
 pip install --user pymediainfo
 pip install --user PySide2
