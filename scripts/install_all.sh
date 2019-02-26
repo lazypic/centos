4df9a59bbc01a400e12ae38bdc14e44b87d6c825
woong <hello@lazypic.org>
2019-02-26 15:04:06 +0900

    등록되면 안되는 파일 추가함.

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
-./nvidia.sh
-./utility.sh
-./code.sh
-./djv.sh
-./mpv.sh
+if [[ $EUID -ne 0 ]]; then
+./root/nvidia.sh
+./root/utility.sh
+./root/code.sh
+./root/djv.sh
+./root/mpv.sh
+./root/rmhotcorner.sh
+./root/devtools.sh
+./root/handbrake.sh
 ./rmbeep.sh
-./trans.sh
-./rmhotcorner.sh
-./pip.sh
-./devtools.sh
-./ffmpeg.sh
-./handbrake.sh
-./openimageio.sh
-./ocio_config.sh
+fi
 
+# install $USER
+./user/trans.sh
+./user/rmbeep.sh
+./user/openimageio.sh
+./user/ocio_config.sh
+./user/ffmpeg.sh
+./user/pip.sh
+./user/rmbeep.sh
 # gcc6.x를 이용해서 빌드한다.
-scl enable devtoolset-6 ./cmake.sh
-scl enable devtoolset-6 ./openexr.sh
-scl enable devtoolset-6 ./opencolorio.sh
-scl enable devtoolset-6 ./alembic.sh
+scl enable devtoolset-6 ./user/cmake.sh
+scl enable devtoolset-6 ./user/openexr.sh
+scl enable devtoolset-6 ./user/opencolorio.sh
+scl enable devtoolset-6 ./user/alembic.sh
