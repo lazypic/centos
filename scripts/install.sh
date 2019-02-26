0134d6abdfcc9ec0a80f0ac011de8d1f5151470d
woong <hello@lazypic.org>
2019-02-26 15:14:13 +0900

    edit install

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,17 +8,19 @@ if [[ $EUID -ne 0 ]]; then
 ./root/rmhotcorner.sh
 ./root/devtools.sh
 ./root/handbrake.sh
+./root/openimageio.sh
+./root/pip_install.sh
 ./rmbeep.sh
 fi
 
 # install $USER
 ./user/trans.sh
-./user/rmbeep.sh
-./user/openimageio.sh
 ./user/ocio_config.sh
 ./user/ffmpeg.sh
 ./user/pip.sh
-./user/rmbeep.sh
+./rmbeep.sh
+./user/gaffer.sh
+./user/blender.sh
 # gcc6.x를 이용해서 빌드한다.
 scl enable devtoolset-6 ./user/cmake.sh
 scl enable devtoolset-6 ./user/openexr.sh
